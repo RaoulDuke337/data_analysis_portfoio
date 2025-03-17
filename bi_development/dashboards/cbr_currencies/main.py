b7995cc4f3bced6a5c9f036c98412b61c81ac49b
nikita.protasov <gomer12370@gmail.com>
2025-03-17 22:33:29 +0300

    unused attributes of the Context class have been removed, the CurrencyFetcher class for SoapServiceFactory has been implemented (also implemented). the Iparser interface has been created, as well as its implementation.

--- bi_development/dashboards/cbr_currencies/main.py
+++ bi_development/dashboards/cbr_currencies/main.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 import json
+from lxml import etree
 import pandas as pd
 import os
 import xml.etree.ElementTree as ET
@@ -21,28 +22,12 @@ class CustomHeaderPlugin(Plugin):
 
 class Context:
     """Объект для хранения общих атрибутов между компонентами"""
-    def __init__(self, days_before, service_name, params={}, separator = ';', extension = '.csv'):
-        self.list_column_name = []
-        self.alt_columns = []
-        self.separator = separator
-        self.extension = extension
-        self.soap_action = ''
-        self.wsdl = ''
-        self.method = ''
-        self.service_name = service_name
-        self.root_tag = ''
-        self.tags = []
-        self.dates = ()
-        self.currency = ''
-        self.currency_code = ''
-        self.days_before = days_before
-        self.df_indexes = pd.DataFrame(columns=self.list_column_name)
-        self.parametrs = params
-        self.query_parametrs = {}
+    def __init__(self, service_name):
         self.username = config('USERNAME')
         self.password = config('PASSWORD')
         self.db_name = config('DB_NAME')
         self.configuration = {}
+        self.service_name = service_name
         self.conn = psycopg2.connect(dbname=self.db_name, user=self.username, password=self.password, host="127.0.0.1")
 
     def get_attr(self, attr: str):
@@ -83,10 +68,16 @@ class IDateSoapClient(ISoapClient):
 
 # 2. Абстракция парсера XML -> DataFrame
 class IParser(ABC):
+    def __init__(self, context: Context):
+        self.root_tag = './' + context.get_attr('root_tag')
+        self.tags = context.get_attr('tags')
+        self.columns = context.get_attr('columns')
+
     @abstractmethod
     def parse(self, xml_data: any) -> pd.DataFrame:
         """Метод для парсинга XML-данных в DataFrame"""
         pass
+        
 
 
 # 3. Абстракция трансформации данных
@@ -130,29 +121,83 @@ class DateSoapClient(IDateSoapClient):
         client = Client(wsdl=self.wsdl, plugins=[CustomHeaderPlugin(self.soap_action)])
         response = getattr(client.service, self.method)(**self.query_parametrs)
         return response
-    
+
+# конкретная реализаци я SOAP-клиента для сервиса currencies 
 class CurrencySoapClient(IDateSoapClient):
     def __init__(self, context: Context):
         super().__init__(context)
         self.parameters = context.get_attr('parametrs')
         self.query_parametrs = None
 
-    def fetch_data(self) -> any:
+    def fetch_data(self, currency_code) -> any:
         self.get_request_date()
         self.query_parametrs = {
             param: value for param, value in zip(self.parameters, self.dates)
         }
-        
+        self.query_parametrs[self.parameters[2]] = currency_code
 
         client = Client(wsdl=self.wsdl, plugins=[CustomHeaderPlugin(self.soap_action)])
         response = getattr(client.service, self.method)(**self.query_parametrs)
         return response
 
+class CurrencyFetcher:
+    """Класс, который управляет запросами для каждой валюты."""
+    
+    def __init__(self, soap_client: CurrencySoapClient, csv_path):
+        self.soap_client = soap_client
+        self.csv_path = csv_path
+        self.currencies = self.load_currencies()
+
+    def load_currencies(self):
+        """Загружает список валют из CSV."""
+        df = pd.read_csv(self.csv_path, sep=';')
+        return df['v_code'].tolist()
+
+    def fetch_all(self):
+        """Итерация по валютам и запрос данных."""
+        results = []
+        for currency in self.currencies:
+            print(f"Извлечение данных для валюты {currency}...")  
+            result = self.soap_client.fetch_data(currency)
+            results.append(result)
+        return results
+
+
+class SoapServiceFactory:
+    """Фабрика для создания SOAP-сервисов"""
+    
+    @staticmethod
+    def create(context: Context):
+        print('Фабрика запущена')
+        service_type = context.get_attr('name')  # Определяем тип сервиса
+        csv_path = './' + context.get_attr('csv_source')
+
+        if service_type == 'currencies':
+            print(f'Источник данных для клиента: {csv_path}, используется CurrencyFetcher')
+            return CurrencyFetcher(CurrencySoapClient(context), csv_path)
+        else:
+            return NoDateSoapClient(context)  # Заглушка для других сервисов
+
 # 6. Конкретный парсер
-class ExampleParser(IParser):
-    def parse(self, xml_data: str) -> pd.DataFrame:
-        # Заглушка для парсинга XML
-        return pd.DataFrame([{"id": 1, "name": "test"}])
+class MainParser(IParser):
+    def __init__(self, context: Context):
+        super().__init__(context)
+
+    def parse(self, xml_data: list) -> pd.DataFrame:
+        data = []
+        for xml_doc in xml_data:
+            tag = xml_doc
+            row = {
+                # извлекаем в root-теге все данные сопоставляя названия столбцов с тегами через dict comp
+                column_name: (tag.find(tag_name).text.strip() if tag.find(tag_name) is not None else None)
+                for column_name, tag_name in zip(self.columns, self.tags)
+            }
+            print(row)
+            data.append(row)
+        print(data)
+        df = pd.DataFrame(data)
+        df.to_csv('./' + 'currencies' + '.csv', index=False, sep=';')
+        return df
 
 
 # 7. Конкретный трансформер
@@ -170,25 +215,43 @@ class ExampleLoader(ILoader):
 
 # 9. Главный обработчик
 class DataPipeline:
-    def __init__(self, client: ISoapClient, parser: IParser, transformer: ITransformer, loader: ILoader):
-        self.client = client
+    """Главный пайплайн для обработки данных"""
+    
+    def __init__(self, service: SoapServiceFactory, parser: IParser, transformer: ITransformer, loader: ILoader, context: Context):
         self.parser = parser
         self.transformer = transformer
         self.loader = loader
+        self.context = context
+        self.service = service.create(context)
 
     def run(self):
-        xml_data = self.client.fetch_data()
-        df = self.parser.parse(xml_data)
-        df = self.transformer.transform(df)
-        self.loader.load(df)
+        """Запускает процесс обработки данных"""
+        if isinstance(self.service, CurrencyFetcher):
+            print('На фабрике выбран экземпляр CurrencyFetcher')
+            results = self.service.fetch_all()
+        else:
+            print('На фабрике выбран экземпляр NoDateSoapClient')
+            results = self.service.fetch_data()
 
+        # results = self.service.fetch_all() if isinstance(self.service, CurrencyFetcher) else [self.service.fetch_data()]
+        
+        parsed = self.parser.parse(results)
+
+        # for result in results:
+            # transformed = self.transformer.transform(parsed)
+            # self.loader.load(transformed)
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(script_dir)
 
 # 🔥 Запуск с разными реализациями
 if __name__ == "__main__":
+    context = Context(service_name="currencies")
     pipeline = DataPipeline(
-        client=ExampleSoapClient(),
-        parser=ExampleParser(),
+        context=context,
+        service=SoapServiceFactory(),
+        parser=MainParser(context),
         transformer=ExampleTransformer(),
-        loader=ExampleLoader()
+        loader=ExampleLoader(),
     )
     pipeline.run()
