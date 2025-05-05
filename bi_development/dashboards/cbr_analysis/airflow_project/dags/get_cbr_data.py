debbdfba9a9cf8b6006cf4520e8fe665fab12b07
nikita.protasov <gomer12370@gmail.com>
2025-05-05 23:03:47 +0300

    fix: added configuration files for airflow, dag: changing mech of creation pipeline object, loader: added a Credential object with env-dependency logic.

--- bi_development/dashboards/cbr_analysis/airflow_project/dags/get_cbr_data.py
+++ bi_development/dashboards/cbr_analysis/airflow_project/dags/get_cbr_data.py
@@ -7,8 +7,6 @@ from cbr_currencies.factory import get_data_pipeline  # Фабрика эээ о
 
 
 
-
-
 def push_dataframe_to_xcom(ti, key, dataframe):
     """Функция для сериализации DataFrame и передачи через XCom"""
     ti.xcom_push(key=key, value=pickle.dumps(dataframe))
@@ -31,51 +29,53 @@ with DAG(
     catchup=False
 ) as dag:
 
+    # Создаем pipeline для каждого сервиса один раз
+    pipelines = {
+        service: get_data_pipeline(service)
+        for service in ["currencies", "metals"]
+    }
+
     def run_fetch(**kwargs):
-        service = kwargs["service"]
-        pipeline = get_data_pipeline(service)
-        kwargs['ti'].xcom_push(key="fetched_data", value=pipeline.soap_client.fetch_data())
+        soap_client = kwargs["soap_client"]
+        kwargs['ti'].xcom_push(key="fetched_data", value=soap_client.fetch_data())
 
     def run_parse(**kwargs):
-        service = kwargs["service"]
-        pipeline = get_data_pipeline(service)
+        parser = kwargs["parser"]
         xml_data = kwargs['ti'].xcom_pull(key="fetched_data")
-        parsed_data = pipeline.parser.parse(xml_data)
+        parsed_data = parser.parse(xml_data)
         push_dataframe_to_xcom(kwargs['ti'], "parsed_data", parsed_data)
 
     def run_transform(**kwargs):
-        service = kwargs["service"]
-        pipeline = get_data_pipeline(service)
+        transformer = kwargs["transformer"]
         parsed_data = pull_dataframe_from_xcom(kwargs['ti'], "parsed_data")
-        transformed_data = pipeline.transformer.transform(parsed_data)
+        transformed_data = transformer.transform(parsed_data)
         push_dataframe_to_xcom(kwargs['ti'], "transformed_data", transformed_data)
 
     def run_load(**kwargs):
-        service = kwargs["service"]
-        pipeline = get_data_pipeline(service)
+        loader = kwargs["loader"]
         data = pull_dataframe_from_xcom(kwargs['ti'], "transformed_data")
-        pipeline.loader.load(data)
+        loader.load(data)
 
-    for service in ["Currencies", "Metals"]:
+    for service, pipeline in pipelines.items():
         fetch = PythonOperator(
             task_id=f"{service}_fetch",
             python_callable=run_fetch,
-            op_kwargs={"service": service}
+            op_kwargs={"soap_client": pipeline.soap_client}
         )
         parse = PythonOperator(
             task_id=f"{service}_parse",
             python_callable=run_parse,
-            op_kwargs={"service": service}
+            op_kwargs={"parser": pipeline.parser}
         )
         transform = PythonOperator(
             task_id=f"{service}_transform",
             python_callable=run_transform,
-            op_kwargs={"service": service}
+            op_kwargs={"transformer": pipeline.transformer}
         )
         load = PythonOperator(
             task_id=f"{service}_load",
             python_callable=run_load,
-            op_kwargs={"service": service}
+            op_kwargs={"loader": pipeline.loader}
         )
-
+         
         fetch >> parse >> transform >> load
--- bi_development/dashboards/cbr_analysis/cbr_currencies/cbr_currencies/core/interfaces/__init__.py
+++ bi_development/dashboards/cbr_analysis/cbr_currencies/cbr_currencies/core/interfaces/__init__.py
@@ -1,10 +1,11 @@
-from .loader_interface import ILoader
+from .loader_interface import ILoader, ICredential
 from .parser_interface import IParser
 from .soap_interface import ISoapClient, IDateSoapClient
 from .transformer_interface import ITransformer
 
 __all__ = [
     "ILoader",
+    "ICredential",
     "IParser",
     "ISoapClient",
     "IDateSoapClient",
